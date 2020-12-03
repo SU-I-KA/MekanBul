ef8a52f9e0d7ca41afcbd9e11a7fac0bab2257e2
SU-I-KA <Bmagdy@mail.com>
2020-12-03 21:53:32 +0300

    versiyon 3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,40 +2,47 @@ var express = require('express');
 var router = express.Router();
 
 const anasayfa=function(req, res, next) {
-  res.render('mekanlar-liste', { title: 'Anasayfa', auther:'Basem Agwa', 'places':[
+  res.render('mekanlar-liste', { 'baslik': 'Anasayfa', 
+  'sayfaBaslik':{
+    'siteAd': 'MekanBul',
+    'aciklama': 'Isparta civarındaki mekanları keşfedin!'
+
+  },
+   auther:'Basem Agwa', 
+   'mekanlar':[
     {
       'ad': 'Starbucks',
       'adres': 'Centrum Garden AVM',
       'puan': '4',
-      'info': ['Dünya kahveleri', 'Kekler', 'Pastalar'],
+      'imkanlar': ['Dünya kahveleri', 'Kekler', 'Pastalar'],
       'mesafe': '10km'
     },
     {
       'ad': 'arbucks',
       'adres': 'CentrumVM',
       'puan': '2',
-      'info': ['Dünya', 'Keer', 'Paslar'],
+      'imkanlar': ['Dünya', 'Keer', 'Paslar'],
       'mesafe': '20km'
     },
     {
       'ad': 'Stucks',
       'adres': 'Iyas',
       'puan': '5',
-      'info': ['kafe', 'Kek', 'Pasta'],
+      'imkanlar': ['kafe', 'Kek', 'Pasta'],
       'mesafe': '12km'
     },
     {
       'ad': 'Star',
       'adres': 'Garden AVM',
       'puan': '3',
-      'info': ['kahve', 'Kek', 'Pastalar'],
+      'imkanlar': ['kahve', 'Kek', 'Pastalar'],
       'mesafe': '8km'
     },
     {
       'ad': 'Bucks',
       'adres': 'Centrum AVM',
       'puan': '4',
-      'info': ['doner', 'Kekler', 'Pasta'],
+      'imkanlar': ['doner', 'Kekler', 'Pasta'],
       'mesafe': '2km'
     }
   ] });
@@ -43,7 +50,58 @@ const anasayfa=function(req, res, next) {
 
 
 const mekanBilgisi=function(req, res, next) {
-  res.render('mekan-detay', { title: 'Mekan Bilgisi', auther:'Basem Agwa' });
+  res.render('mekan-detay', 
+  { baslik: 'Mekan Bilgisi', 'sayfaBaslik':'Starbucks', auther:'Basem Agwa',
+  'mekanBilgisi':{
+    'ad': 'Starbucks',
+    'adres': 'Centrum Garden AVM',
+    'puan': '4',
+    'imkanlar': ['Dünya kahveleri', 'Kekler', 'Pastalar'],
+    'koordinatlar':{
+      'enlem': '37.781885',
+      'boylam': '30.566034'
+    },
+    'saatler': [
+      {
+        'gunler': 'Pazartesi-Cuma',
+        'acilis': '7:00',
+        'kapanis': '23:00',
+        'kapali': false
+      },
+      {
+        'gunler': 'Cumartesi',
+        'acilis': '9:00',
+        'kapanis': '22:00',
+        'kapali': false
+      },
+      {
+        'gunler': 'Pazar',
+        'kapali': true
+      }
+    ],
+    'yorumlar':[
+      {
+        'yorumYapan': 'Basem Agwa',
+        'puan':'3',
+        'tarih': '27.11.2020',
+        'yorumMetini': 'Kahveleri guzel.'
+      },
+      {
+        'yorumYapan': 'Ahmet ozturk',
+        'puan':'4',
+        'tarih': '27.11.2020',
+        'yorumMetini': 'Kahveleri guzel.'
+      },
+      {
+        'yorumYapan': 'Furkan Yaman',
+        'puan':'5',
+        'tarih': '27.11.2020',
+        'yorumMetini': 'Kahveleri guzel.'
+      }
+    ]
+  }
+
+});
 };
 
 
